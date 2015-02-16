3d2e92bf92c3ede3713965c1f8f2bca23b6dcf8e
Chip Irek <chip.irek@gmail.com>
2015-02-16 14:00:34 -0500

    Completed wiring up Angular to send mark-complete and mark-incomplete async PUTs to the API when the use clicks the checkbox

--- ionicApp.js
+++ ionicApp.js
@@ -62,25 +62,93 @@ angular.module('ionicApp', ['ionic'])
 
     })
 
-    .controller('TodayController', function ($scope, $ionicSideMenuDelegate, $timeout) {
-        $scope.items = [
-            { text: "clean downstairs", checked: true },
-            { text: "work on training Molly", checked: false },
-            { text: "pick the backyard / clean garage / clean barn", checked: false }
-        ];
+    .controller('TodayController', function ($scope, $ionicSideMenuDelegate, $timeout, $http) {
 
-        //$scope.toggleLeft = function() {
-        //  $ionicSideMenuDelegate.toggleLeft();
-        //};
+        function getData()
+        {
+            $scope.items = [];
+            $http( { method: 'GET', $$asyncCallback:false,
+                url: 'http://myday.herokuapp.com/today.json?auth_token=mjwiPdGEPqnAV8MiFfLp',
+                headers: {'X-Requested-With': 'XMLHttpRequest', 'Accept': 'application/json, text/plain, */*'}})
+                .success(function(data, status, headers, config)
+                {
+                    console.log('$http success');
+                    angular.forEach(data, function(todo)
+                    {
+                        var item = { id: todo.id, text: todo.subject, checked: todo.is_complete };
+                        $scope.items.push(item);
+                    });
+                    //cordova.plugins.notification.badge.set($scope.incompleteItems);
+                })
+                .error(function(data, status, headers, config) {
+                    alert('error in $http get');
+                    console.log('error in $http get');
+                    console.log('status=' + status);
+                    console.log('headers=' + headers);
+                    console.log('config=' + config);
+                });
 
-        $scope.doRefresh = function () {
+        }
 
-            console.log('Refreshing!');
+
+        function markComplete(id)
+        {
+            $http({
+                method: 'PUT',
+                url: 'http://myday.herokuapp.com/todos/mark_complete/' + id + '?auth_token=mjwiPdGEPqnAV8MiFfLp',
+                headers: {'X-Requested-With': 'XMLHttpRequest', 'Accept': 'application/json, text/plain, */*'}})
+                .success(function(data, status, headers, config)
+                {
+                    // do something?
+                })
+                .error(function(data, status, headers, config)
+                {
+                    // do something?
+                    console.log('error in $http get');
+                    console.log('status=' + status);
+                    console.log('headers=' + headers);
+                    console.log('config=' + config);
+                })
+                .finally(function(data, status, headers, config)
+                {
+                    // do something?
+                });
+
+        }
+
+
+        function markIncomplete(id)
+        {
+            $http({
+                method: 'PUT',
+                url: 'http://myday.herokuapp.com/todos/mark_incomplete/' + id + '?auth_token=mjwiPdGEPqnAV8MiFfLp',
+                headers: {'X-Requested-With': 'XMLHttpRequest', 'Accept': 'application/json, text/plain, */*'}})
+                .success(function(data, status, headers, config)
+                {
+                    // do something?
+                })
+                .error(function(data, status, headers, config)
+                {
+                    // do something?
+                    console.log('error in $http get');
+                    console.log('status=' + status);
+                    console.log('headers=' + headers);
+                    console.log('config=' + config);
+                })
+                .finally(function(data, status, headers, config)
+                {
+                    // do something?
+                });
+
+        }
+
+
+        $scope.doRefresh = function ()
+        {
+
+            console.log('user initiated refreshing...');
             $timeout(function () {
-                //simulate async response
-                var s = 'New Item ' + Math.floor(Math.random() * 1000) + 4;
-                $scope.items.push({ text: s, checked: false });
-
+                getData();
                 //Stop the ion-refresher from spinning
                 $scope.$broadcast('scroll.refreshComplete');
 
@@ -88,20 +156,269 @@ angular.module('ionicApp', ['ionic'])
 
         };
 
+
+        $scope.check = function(id)
+        {
+
+            angular.forEach($scope.items, function(o)
+            {
+                if (o.id == id)
+                {
+                    if (o.checked == false)
+                    {
+                        markIncomplete(id);
+                    }
+                    else
+                    {
+                        markComplete(id);
+                    }
+                }
+            });
+
+        }
+
+
+        // do this to load the page data
+        getData();
+
     })
 
-    .controller('TomorrowController', function ($scope) {
-        //alert('got in tomorrow');
-        $scope.items = [
-            { text: "clean downstairs", checked: true },
-            { text: "work on training Molly", checked: false },
-            { text: "pick the backyard / clean garage / clean barn", checked: false }
-        ];
+    .controller('TomorrowController', function ($scope, $ionicSideMenuDelegate, $timeout, $http) {
+
+        function getData()
+        {
+            $scope.items = [];
+            $http( { method: 'GET', $$asyncCallback:false,
+                url: 'http://myday.herokuapp.com/tomorrow.json?auth_token=mjwiPdGEPqnAV8MiFfLp',
+                headers: {'X-Requested-With': 'XMLHttpRequest', 'Accept': 'application/json, text/plain, */*'}})
+                .success(function(data, status, headers, config)
+                {
+                    console.log('$http success');
+                    angular.forEach(data, function(todo)
+                    {
+                        var item = { id: todo.id, text: todo.subject, checked: todo.is_complete };
+                        $scope.items.push(item);
+                    });
+                    //cordova.plugins.notification.badge.set($scope.incompleteItems);
+                })
+                .error(function(data, status, headers, config) {
+                    console.log('error in $http get');
+                    console.log('status=' + status);
+                    console.log('headers=' + headers);
+                    console.log('config=' + config);
+                });
+
+        }
+
+
+        function markComplete(id)
+        {
+            $http({
+                method: 'PUT',
+                url: 'http://myday.herokuapp.com/todos/mark_complete/' + id + '?auth_token=mjwiPdGEPqnAV8MiFfLp',
+                headers: {'X-Requested-With': 'XMLHttpRequest', 'Accept': 'application/json, text/plain, */*'}})
+                .success(function(data, status, headers, config)
+                {
+                    // do something?
+                })
+                .error(function(data, status, headers, config)
+                {
+                    // do something?
+                    console.log('error in $http get');
+                    console.log('status=' + status);
+                    console.log('headers=' + headers);
+                    console.log('config=' + config);
+                })
+                .finally(function(data, status, headers, config)
+                {
+                    // do something?
+                });
+
+        }
+
+
+        function markIncomplete(id)
+        {
+            $http({
+                method: 'PUT',
+                url: 'http://myday.herokuapp.com/todos/mark_incomplete/' + id + '?auth_token=mjwiPdGEPqnAV8MiFfLp',
+                headers: {'X-Requested-With': 'XMLHttpRequest', 'Accept': 'application/json, text/plain, */*'}})
+                .success(function(data, status, headers, config)
+                {
+                    // do something?
+                })
+                .error(function(data, status, headers, config)
+                {
+                    // do something?
+                    console.log('error in $http get');
+                    console.log('status=' + status);
+                    console.log('headers=' + headers);
+                    console.log('config=' + config);
+                })
+                .finally(function(data, status, headers, config)
+                {
+                    // do something?
+                });
+
+        }
+
+
+        $scope.doRefresh = function ()
+        {
+
+            console.log('user initiated refreshing...');
+            $timeout(function () {
+                getData();
+                //Stop the ion-refresher from spinning
+                $scope.$broadcast('scroll.refreshComplete');
+
+            }, 1000);
+
+        };
+
+
+        $scope.check = function(id)
+        {
+
+            angular.forEach($scope.items, function(o)
+            {
+                if (o.id == id)
+                {
+                    if (o.checked == false)
+                    {
+                        markIncomplete(id);
+                    }
+                    else
+                    {
+                        markComplete(id);
+                    }
+                }
+            });
+
+        }
+
+
+        // do this to load the page data
+        getData();
 
     })
 
-    .controller('SomedayController', function ($scope, $state) {
-        //alert('got in someday');
-        console.log('Simulating that no auth token is present and we say NO you better login...');
-        $state.go('signin');
+    .controller('SomedayController', function ($scope, $ionicSideMenuDelegate, $timeout, $http) {
+
+        function getData()
+        {
+            $scope.items = [];
+            $http( { method: 'GET', $$asyncCallback:false,
+                url: 'http://myday.herokuapp.com/backlog.json?auth_token=mjwiPdGEPqnAV8MiFfLp',
+                headers: {'X-Requested-With': 'XMLHttpRequest', 'Accept': 'application/json, text/plain, */*'}})
+                .success(function(data, status, headers, config)
+                {
+                    console.log('$http success');
+                    angular.forEach(data, function(todo)
+                    {
+                        var item = { id: todo.id, text: todo.subject, checked: todo.is_complete };
+                        $scope.items.push(item);
+                    });
+                    //cordova.plugins.notification.badge.set($scope.incompleteItems);
+                })
+                .error(function(data, status, headers, config) {
+                    console.log('error in $http get');
+                    console.log('status=' + status);
+                    console.log('headers=' + headers);
+                    console.log('config=' + config);
+                });
+
+        }
+
+
+        function markComplete(id)
+        {
+            $http({
+                method: 'PUT',
+                url: 'http://myday.herokuapp.com/todos/mark_complete/' + id + '?auth_token=mjwiPdGEPqnAV8MiFfLp',
+                headers: {'X-Requested-With': 'XMLHttpRequest', 'Accept': 'application/json, text/plain, */*'}})
+                .success(function(data, status, headers, config)
+                {
+                    // do something?
+                })
+                .error(function(data, status, headers, config)
+                {
+                    // do something?
+                    console.log('error in $http get');
+                    console.log('status=' + status);
+                    console.log('headers=' + headers);
+                    console.log('config=' + config);
+                })
+                .finally(function(data, status, headers, config)
+                {
+                    // do something?
+                });
+
+        }
+
+
+        function markIncomplete(id)
+        {
+            $http({
+                method: 'PUT',
+                url: 'http://myday.herokuapp.com/todos/mark_incomplete/' + id + '?auth_token=mjwiPdGEPqnAV8MiFfLp',
+                headers: {'X-Requested-With': 'XMLHttpRequest', 'Accept': 'application/json, text/plain, */*'}})
+                .success(function(data, status, headers, config)
+                {
+                    // do something?
+                })
+                .error(function(data, status, headers, config)
+                {
+                    // do something?
+                    console.log('error in $http get');
+                    console.log('status=' + status);
+                    console.log('headers=' + headers);
+                    console.log('config=' + config);
+                })
+                .finally(function(data, status, headers, config)
+                {
+                    // do something?
+                });
+
+        }
+
+
+        $scope.doRefresh = function ()
+        {
+
+            console.log('user initiated refreshing...');
+            $timeout(function () {
+                getData();
+                //Stop the ion-refresher from spinning
+                $scope.$broadcast('scroll.refreshComplete');
+
+            }, 1000);
+
+        };
+
+
+        $scope.check = function(id)
+        {
+
+            angular.forEach($scope.items, function(o)
+            {
+                if (o.id == id)
+                {
+                    if (o.checked == false)
+                    {
+                        markIncomplete(id);
+                    }
+                    else
+                    {
+                        markComplete(id);
+                    }
+                }
+            });
+
+        }
+
+
+        // do this to load the page data
+        getData();
+
     });
