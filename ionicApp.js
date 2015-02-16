76c3b2205ee80f61039094d7d1bf0423d76984eb
Chip Irek <chip.irek@gmail.com>
2015-02-16 09:47:57 -0500

    Set up ionic assets locally - outside of a full "build" environment

--- ionicApp.js
+++ ionicApp.js
@@ -1,107 +1,107 @@
 angular.module('ionicApp', ['ionic'])
 
-.config(function($stateProvider, $urlRouterProvider) {
+    .config(function ($stateProvider, $urlRouterProvider) {
 
-  $stateProvider
-	.state('signin', {
-      url: '/signin',
-      templateUrl: 'templates/signin.html',
-      controller: 'SignInController'
+        $stateProvider
+            .state('signin', {
+                url: '/signin',
+                templateUrl: 'templates/signin.html',
+                controller: 'SignInController'
+            })
+            .state('appmenu', {
+                url: "/tasks",
+                abstract: true,
+                templateUrl: "templates/task-menu.html"
+            })
+            .state('appmenu.home', {
+                url: "/home",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/home.html"
+                    }
+                }
+            })
+            .state('appmenu.today', {
+                url: "/today",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/today.html",
+                        controller: "TodayController"
+                    }
+                }
+            })
+            .state('appmenu.tomorrow', {
+                url: "/tomorrow",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/tomorrow.html",
+                        controller: "TomorrowController"
+                    }
+                }
+            })
+            .state('appmenu.someday', {
+                url: "/someday",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/someday.html",
+                        controller: "SomedayController"
+                    }
+                }
+            })
+
+        $urlRouterProvider.otherwise("/tasks/home");
     })
-    .state('appmenu', {
-      url: "/tasks",
-      abstract: true,
-      templateUrl: "templates/task-menu.html"
+
+    .controller('SignInController', function ($scope, $state) {
+
+        $scope.signIn = function (user) {
+            console.log('username=', user.username);
+            console.log('password=', user.password);
+            $state.go('appmenu.home');
+        };
+
     })
-    .state('appmenu.home', {
-      url: "/home",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/home.html"
-        }
-      }
+
+    .controller('TodayController', function ($scope, $ionicSideMenuDelegate, $timeout) {
+        $scope.items = [
+            { text: "clean downstairs", checked: true },
+            { text: "work on training Molly", checked: false },
+            { text: "pick the backyard / clean garage / clean barn", checked: false }
+        ];
+
+        //$scope.toggleLeft = function() {
+        //  $ionicSideMenuDelegate.toggleLeft();
+        //};
+
+        $scope.doRefresh = function () {
+
+            console.log('Refreshing!');
+            $timeout(function () {
+                //simulate async response
+                var s = 'New Item ' + Math.floor(Math.random() * 1000) + 4;
+                $scope.items.push({ text: s, checked: false });
+
+                //Stop the ion-refresher from spinning
+                $scope.$broadcast('scroll.refreshComplete');
+
+            }, 1000);
+
+        };
+
     })
-    .state('appmenu.today', {
-      url: "/today",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/today.html",
-          controller: "TodayController"
-        }
-      }
+
+    .controller('TomorrowController', function ($scope) {
+        //alert('got in tomorrow');
+        $scope.items = [
+            { text: "clean downstairs", checked: true },
+            { text: "work on training Molly", checked: false },
+            { text: "pick the backyard / clean garage / clean barn", checked: false }
+        ];
+
     })
-    .state('appmenu.tomorrow', {
-      url: "/tomorrow",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/tomorrow.html",
-          controller: "TomorrowController"
-        }
-      }
-    })
-    .state('appmenu.someday', {
-      url: "/someday",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/someday.html",
-          controller: "SomedayController"
-        }
-      }
-    })
-  
-  $urlRouterProvider.otherwise("/tasks/home");
-})
 
-.controller('SignInController', function($scope, $state) {
-  
-  $scope.signIn = function(user) {
-    console.log('username=', user.username);
-	console.log('password=', user.password);
-    $state.go('appmenu.home');
-  };
-  
-})
-
-.controller('TodayController', function($scope, $ionicSideMenuDelegate, $timeout) {
-  $scope.items = [
-    { text: "clean downstairs", checked: true },
-    { text: "work on training Molly", checked: false },
-    { text: "pick the backyard / clean garage / clean barn", checked: false }
-  ];
-  
-  //$scope.toggleLeft = function() {
-  //  $ionicSideMenuDelegate.toggleLeft();
-  //};
-
-  $scope.doRefresh = function() {
-    
-    console.log('Refreshing!');
-    $timeout( function() {
-      //simulate async response
-	  var s = 'New Item ' + Math.floor(Math.random() * 1000) + 4;
-      $scope.items.push({ text: s, checked: false });
-
-      //Stop the ion-refresher from spinning
-      $scope.$broadcast('scroll.refreshComplete');
-    
-    }, 1000);
-      
-  };
-
-})
-
-.controller('TomorrowController', function($scope) {
-	//alert('got in tomorrow');
-	$scope.items = [
-    { text: "clean downstairs", checked: true },
-    { text: "work on training Molly", checked: false },
-    { text: "pick the backyard / clean garage / clean barn", checked: false }
-  ];
-  
-})
-
-.controller('SomedayController', function($scope, $state) {
-	//alert('got in someday');
-	console.log('Simulating that no auth token is present and we say NO you better login...');
-	$state.go('signin');
-});
+    .controller('SomedayController', function ($scope, $state) {
+        //alert('got in someday');
+        console.log('Simulating that no auth token is present and we say NO you better login...');
+        $state.go('signin');
+    });
